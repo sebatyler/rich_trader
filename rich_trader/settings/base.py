85aef5455a751a77cd4b47e0f8ad803deff2d5a8
sebatyler <sebatyler@gmail.com>
2025-03-13 11:28:52 +0900

    feat: set django settings module for crontab

--- rich_trader/settings/base.py
+++ rich_trader/settings/base.py
@@ -205,3 +205,4 @@ CRONJOBS = [
     ("*/5 * * * *", "rich.service.buy_upbit_coins", ">> /tmp/buy_upbit_coins.log 2>&1"),
 ]
 CRONTAB_COMMAND_PREFIX = "USE_DB_URL=1"
+CRONTAB_DJANGO_SETTINGS_MODULE = "rich_trader.settings.prod"
